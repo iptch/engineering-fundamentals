93393d512526933ca46506a337ec6ad85e186e67
joeipt <joe.mueller@ipt.ch>
2026-04-14 07:50:51 +0000

    feat(JOE): Added Unit Test

--- src/__tests__/Counter.test.tsx
+++ src/__tests__/Counter.test.tsx
@@ -1,7 +1,18 @@
-import { render} from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import Counter from "../Counter";
 
 test("increments count when button is clicked", () => {
+    
     render(<Counter />);
-    // Add the test logic below
+
+  // Find the button
+  const button = screen.getByRole("button", { name: /count is 0/i });
+
+  // Click it once
+  fireEvent.click(button);
+  expect(button).toHaveTextContent("count is 1");
+
+  // Click it again
+  fireEvent.click(button);
+  expect(button).toHaveTextContent("count is 2");
 });
