4875d06e087a8f070e5ae8a85a2b94bfc3025545
mpr-ipt <marlene.praeauer@ipt.ch>
2026-04-14 08:00:12 +0000

    Add test and workflows file

--- src/__tests__/Counter.test.tsx
+++ src/__tests__/Counter.test.tsx
@@ -1,7 +1,17 @@
-import { render} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Counter from "../Counter";
 
-test("increments count when button is clicked", () => {
-    render(<Counter />);
-    // Add the test logic below
+test("increments count when button is clicked", async () => {
+  const user = userEvent.setup();
+  render(<Counter />);
+
+  const button = screen.getByRole("button", { name: /count is 0/i });
+  expect(button).toBeInTheDocument();
+
+  await user.click(button);
+  expect(screen.getByRole("button", { name: /count is 1/i })).toBeInTheDocument();
+
+  await user.click(button);
+  expect(screen.getByRole("button", { name: /count is 2/i })).toBeInTheDocument();
 });
