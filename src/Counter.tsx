797d787283176f8115544a100226142f1e42d7fd
badrou <badr.outiti@hotmail.com>
2026-07-28 13:16:19 +0200

    test

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
 
+    // Diese Funktion wird nirgendwo importiert!
+    export function calculateObsoleteTax(amount: number): number {
+        return amount * 0.19;
+    }
+
   return (
     <div className="card">
       <button onClick={() => setCount((count) => count + 1)}>
